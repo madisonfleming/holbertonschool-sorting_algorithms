0be0af6751284bb00976673c86c2c770528e68d4
madisonfleming <maddyyfleming@gmail.com>
2025-08-09 00:25:10 +0000

    file2

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * selection_sort - sort an array of integers using selection sort
+ * @array: pointer to the array
+ * @size: size of the array
+ * Return: sorted array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, min;
+	int temp;
+
+	while (i < size - 1)
+	{
+		min = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+			j++;
+		}
+		temp = array[min];
+		array[min] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+		i++;
+	}
+}

f081f5c6a3af9a9ce8b5d5d41c0a5b47b9faccbf
madisonfleming <maddyyfleming@gmail.com>
2025-08-11 03:58:30 +0000

    file3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,6 +2,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * swap - swap two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: success
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * lomuto_partition - swap values using lomuto partition
+ * @array: the array to partition
+ * @low: starting index
+ * @high: ending index
+ * @size: full array size
+ * Return: final pivot position
+ */
+size_t lomuto_partition(int *array, size_t low, size_t high, size_t size)
+{
+	int pivot = array[high];
+	size_t i = low;
+	size_t j = low;
+
+	while (j < high)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+			i++;
+		}
+		j++;
+	}
+	if (i != high)
+	{
+		swap(&array[i], &array[high]);
+		print_array(array, size);
+	}
+	return (i);
+}
+
+/**
+ * quick_sort_recursion - recursive quicksort
+ * @array: array to sort
+ * @low: starting index
+ * @high: ending index
+ * @size: full array size
+ * Return: sorted array
+ */
+void quick_sort_recursion(int *array, size_t low, size_t high, size_t size)
+{
+	if (low < high)
+	{
+		size_t pi = lomuto_partition(array, low, high, size);
+
+		if (pi > 0)
+			quick_sort_recursion(array, low, pi - 1, size);
+		quick_sort_recursion(array, pi + 1, high, size);
+	}
+}
+
+/**
  * quick_sort - sort an array using quicksort
  * @array: array to sort
  * @size: size of array
@@ -9,24 +77,8 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	int pivot = array[size - 1];
-	int i = -1;
-	size_t j = 0;
-	int temp;
+	if (!array || size < 2)
+		return;
 
-	while (j < size)
-	{
-	
-		if (array[j] < pivot)
-		{
-			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-		}
-		print_array(array, size);
-	}
-	temp = array[i + 1];
-	array[i + 1] = array[size - 1];
-	array[size - 1] = temp;
+	quick_sort_recursion(array, 0, size - 1, size);
 }
